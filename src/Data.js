1a4635f7da3580cedfe72c5d4be37bc746d33800
ehsan <ehsanmavaei@gmail.com>
2021-05-06 18:20:54 +0430

    Add the Data file

---
+++ src/Data.js
@@ -0,0 +1,43 @@
+export default [
+    {
+      id: 1,
+      title: 'MacBook Air MWTJ2 2020',
+      price: 599.99,
+      img:
+        'https://res.cloudinary.com/de8iseeez/image/upload/v1620306534/MacBook_Air_MWTK2_2020_yqa0gu.jpg',
+      amount: 1,
+    },
+    {
+      id: 2,
+      title: 'MacBook Air MGND3 2020 ',
+      price: 499.99,
+      img:
+        'https://res.cloudinary.com/de8iseeez/image/upload/v1620306533/MacBook_Air_MGND3_2020_pmpme7.jpg',
+      amount: 1,
+    },
+    {
+      id: 3,
+      title: ' Dell XPS 9300',
+      price: 699.99,
+      img:
+        'https://res.cloudinary.com/de8iseeez/image/upload/v1620306535/XPS_9300_qamxll.jpg',
+      amount: 1,
+    },
+    {
+      id: 4,
+      title: ' ASUS A515-55G-72HH',
+      price: 699.99,
+      img:
+        'https://res.cloudinary.com/de8iseeez/image/upload/v1620306533/A515-55G-72HH_qyfbaz.jpg',
+      amount: 1,
+    },
+    {
+      id: 5,
+      title: ' huawei MateBook X Pro MACHC-WAE9LP - A',
+      price: 699.99,
+      img:
+        'https://res.cloudinary.com/de8iseeez/image/upload/v1620306535/MateBook_X_Pro_MACHC-WAE9LP_-_A_qorcb5.jpg',
+      amount: 1,
+    },
+  ];
+  
