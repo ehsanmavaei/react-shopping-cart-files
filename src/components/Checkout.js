f121a6cfe48dee6f66d2087b990beebe296cc0d6
ehsan <ehsanmavaei@gmail.com>
2021-05-08 09:22:06 +0430

    Add the chekout section

---
+++ src/components/Checkout.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Checkout = () => {
+    return (
+        <div className="checkout">
+            <h3>totall (5): $120400</h3>
+            <hr/>
+            <button className="cursor">Pay</button>
+
+
+        </div>
+        
+        
+        )
+}
+
+export default Checkout
